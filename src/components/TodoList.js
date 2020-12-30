5e1ab25457f08bf903c2ffe8daa240845313255e
Ivan Barba <ivanbarba@canesstudio.com>
2020-12-29 23:09:59 -0600

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+//components
+import Todo from './Todo'
+
+const TodoList = ({ setTodos, todos, filteredTodos }) => {
+    console.log(todos)
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map((todo) => (
+                    <Todo
+                        setTodos={setTodos}
+                        todos={todos}
+                        key={todo.id}
+                        text={todo.text}
+                        todo={todo}
+                    />
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList
